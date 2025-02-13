6025200b48e56fe3ca6848f2377a013d11df9cf9
Anushka Purwar - Broadway Infotech <anushka.p@broadwayinfotech.com>
2025-02-13 17:37:47 +0530

    14-feb

--- app/(dashboard)/(routes)/home/CardSection.jsx
+++ app/(dashboard)/(routes)/home/CardSection.jsx
@@ -1,39 +1,68 @@
 'use client';
-import 'font-awesome/css/font-awesome.min.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolderOpen, faChartBar } from "@fortawesome/free-regular-svg-icons";
+import { faChartLine, faLaptop, faRankingStar } from "@fortawesome/free-solid-svg-icons";
 
 const CardSection = () => {
     return (
         <div className="flex flex-col items-center justify-center bg-cover bg-center text-[#170F00] py-10 px-4 sm:px-6 lg:px-10 xl:px-20">
             {/* Title and Subtitle */}
-            <h2 className="text-[22px] sm:text-[28px] md:text-[36px] lg:text-[42px] xl:text-[48px] font-bold mb-4 font-poppins text-center leading-tight max-w-[90%]">
+            <h2 className="text-[22px] sm:text-[28px] md:text-[36px] lg:text-[42px] xl:text-[48px] font-bold mb-4 font-poppins text-center leading-tight max-w-[90%] text-[#804000]">
                 Accelerate Your Career <br /> in Data Analytics
             </h2>
-            <p className="text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px] text-center font-regular max-w-[90%] sm:max-w-[700px] lg:max-w-[850px] xl:max-w-[1000px] w-full mx-auto font-poppins mb-10">
+            <p className="text-[13px] sm:text-[15px] md:text-[16px] lg:text-[18px] text-center font-regular max-w-[90%] sm:max-w-[700px] lg:max-w-[850px] xl:max-w-[1000px] w-full mx-auto font-poppins mb-10 text-[#170f00ec] ">
                 Learn with Purpose, Grow with Confidence
             </p>
 
             {/* Card & Image Container */}
             <div className="flex flex-wrap sm:flex-nowrap items-center justify-center w-full gap-8 px-4 sm:px-6 lg:px-10">
                 {/* Left Cards */}
-                <div className="flex flex-col gap-6 w-full sm:w-1/3 items-center">
+                <div className="flex flex-col gap-6 w-full sm:w-1/3 items-center ">
                     {/** Point 1 **/}
-                    <div className="p-6 border-2 rounded-lg w-full sm:w-[300px] h-[260px] flex flex-col justify-between transition-all duration-300 hover:scale-105 hover:transform shadow-lg text-center">
-                        <div className="flex items-center justify-center text-4xl sm:text-5xl text-[#170F00] mt-2">
-                            💻
-                            <i class="fa-solid fa-chart-line text-4xl text-[#FAA51D] mb-6"></i>
+                    <div
+                        class="bg-white w-[300px] rounded-[30px] flex flex-col justify-center shadow-lg hover:shadow-lg min-h-[280px] dark:bg-gray-800 dark:text-white items-start relative group"
+                    >
+                        <div class="m-5">
+                            <div
+                                class="w-12 h-12 flex items-center justify-center absolute inset-x-0 top-0 ml-6 mt-6"
+                            >
+                            </div>
+
+                            <div className="mt-4 text-left w-full mb-3">
+                                <h2 className="text-2xl roboto-mono-500 text-gray-800 dark:text-white">
+                                Complete Curriculum
+                                </h2>
+                                <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
+                                Master <b>MySQL</b>, <b>PostgreSQL</b>, <b>Tableau</b>, <b>Power BI</b>, <b>Python</b>—all in one place.
+                                </p>
+                            </div>
+                            <a href="#">
+                                <div
+                                    className="bg-gray-300 dark:bg-gray-700 w-10 h-10 rounded-full absolute bottom-0 left-0 m-4 flex justify-center items-center hover:ring-4 ring-gray-200 dark:ring-gray-400 hover:transition duration-700 ease-in-out"
+                                >
+                                    
+                                </div>
+                            </a>
                         </div>
-                        <h3 className="text-[17px] sm:text-[19px] font-semibold mb-2">Complete Curriculum</h3>
-                        <p className="text-[14px] sm:text-[16px]">
-                            Master <b>MySQL</b>, <b>PostgreSQL</b>, <b>Tableau</b>, <b>Power BI</b>, <b>Python</b>—all in one place.
-                        </p>
                     </div>
 
-                    {/** Point 2 **/}
-                    <div className="p-6 border-2 rounded-lg w-full sm:w-[300px] h-[260px] flex flex-col justify-between transition-all duration-300 hover:scale-105 hover:transform shadow-lg text-center">
-                        <div className="flex items-center justify-center text-4xl sm:text-5xl text-[#170F00] mt-2">
-                            📊
+                    {/* <div className="p-6 rounded-lg w-full sm:w-[300px] h-[260px] flex flex-col justify-between transition-all duration-300 hover:scale-105 hover:transform shadow-lg text-center">
+                        <div className="flex items-center justify-center text-4xl sm:text-5xl text-[#170F00] mt-2 bg-[]">
+                            <FontAwesomeIcon icon={faChartBar} className="text-5xl text-[#ffbd30] mb-6 bg-[#ffd88459] p-3 rounded-2xl" />
+
                         </div>
-                        <h3 className="text-[17px] sm:text-[19px] font-semibold mb-2">Hands-On Projects</h3>
+                        <h3 className="text-[17px] sm:text-[19px] font-semibold mb-2 text-[#170f00fa]">Complete Curriculum</h3>
+                        <p className="text-[14px] sm:text-[16px] text-[#170f00ec]">
+                            Master <b>MySQL</b>, <b>PostgreSQL</b>, <b>Tableau</b>, <b>Power BI</b>, <b>Python</b>—all in one place.
+                        </p>
+                    </div> */}
+
+                    {/** Point 2 **/}
+                    <div className="p-6 rounded-lg w-full sm:w-[300px] h-[260px] flex flex-col justify-between transition-all duration-300 hover:scale-105 hover:transform shadow-lg text-center">
+                        <div className="flex items-center justify-center text-4xl sm:text-5xl text-[#170F00] mt-2">
+                            <FontAwesomeIcon icon={faFolderOpen} className="text-5xl text-[#ffbd30] mb-6 bg-[#ffd88459] p-3 rounded-2xl" />
+                        </div>
+                        <h3 className="text-[17px] sm:text-[19px] font-semibold mb-2 text-[#170f00fa]">Hands-On Projects</h3>
                         <p className="text-[14px] sm:text-[16px]">From basics to real-world tasks, build your skills step-by-step.</p>
                     </div>
                 </div>
@@ -50,31 +79,26 @@ const CardSection = () => {
                 {/* Right Cards */}
                 <div className="flex flex-col gap-6 w-full sm:w-1/3 items-center">
                     {/** Point 3 **/}
-                    <div className="p-6 border-2 rounded-lg w-full sm:w-[300px] h-[260px] flex flex-col justify-between transition-all duration-300 hover:scale-105 hover:transform shadow-lg text-center">
-                        <div className="flex items-center justify-center text-4xl sm:text-5xl text-[#170F00] mt-2">
-                            🔥
+                    <div className="p-6 border-1 rounded-lg w-full sm:w-[300px] h-[260px] flex flex-col justify-between transition-all duration-300 hover:scale-105 hover:transform shadow-lg text-center">
+                        <div className="flex items-center justify-center text-4xl sm:text-5xl text-[#170F00] mt-2 ">
+                            <FontAwesomeIcon icon={faLaptop} className="text-5xl text-[#ffbd30] mb-6 bg-[#ffd88459] p-3 rounded-2xl" />
                         </div>
-                        <h3 className="text-[17px] sm:text-[19px] font-semibold mb-2">Interactive Practice</h3>
-                        <p className="text-[14px] sm:text-[16px]">Tackle real scenarios and practice with industry-focused exercises.</p>
+                        <h3 className="text-[17px] sm:text-[19px] font-semibold mb-2 text-[#170f00fa]">Interactive Practice</h3>
+                        <p className="text-[14px] sm:text-[16px] text-[#170f00fa]">Tackle real scenarios and practice with industry-focused exercises.</p>
                     </div>
 
                     {/** Point 4 **/}
                     <div className="p-6 border-2 rounded-lg w-full sm:w-[300px] h-[260px] flex flex-col justify-between transition-all duration-300 hover:scale-105 hover:transform shadow-lg text-center">
                         <div className="flex items-center justify-center text-4xl sm:text-5xl text-[#170F00] mt-2">
-                            🎯
+                            <FontAwesomeIcon icon={faRankingStar} className="text-5xl text-[#ffbd30] mb-6 bg-[#ffd88459] p-3 rounded-2xl" />
                         </div>
-                        <h3 className="text-[17px] sm:text-[19px] font-semibold mb-2">Interview-Ready</h3>
-                        <p className="text-[14px] sm:text-[16px]">Prepare with level-wise questions tailored for top companies.</p>
+                        <h3 className="text-[17px] sm:text-[19px] font-semibold mb-2 text-[#170f00fa]">Interview-Ready</h3>
+                        <p className="text-[14px] sm:text-[16px] text-[#170f00fa]">Prepare with level-wise questions tailored for top companies.</p>
                     </div>
                 </div>
             </div>
 
-            {/* Button */}
-            <div className="mt-10">
-                <button className="bg-[#FAA51D] text-white text-[14px] sm:text-[16px] font-bold px-6 sm:px-10 py-2 sm:py-3 rounded-lg hover:bg-[#FF9D00] transition-all duration-300 font-poppins">
-                    Learn More
-                </button>
-            </div>
+
         </div>
     );
 };
--- app/(dashboard)/(routes)/home/HeroSection.jsx
+++ app/(dashboard)/(routes)/home/HeroSection.jsx
@@ -44,7 +44,7 @@ const HeroSection = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.8, delay: 0.4 }}
-          className="text-[16px] sm:text-[18px] md:text-[18px] lg:text-[16px] text-[#170F00] font-regular max-w-[90%] sm:max-w-[700px] lg:max-w-[850px] xl:max-w-[900px] w-full mx-auto mt-2 text-center font-poppins leading-relaxed"
+          className="text-[16px] sm:text-[18px] md:text-[18px] lg:text-[16px] text-[#170f00ec] font-regular max-w-[90%] sm:max-w-[700px] lg:max-w-[850px] xl:max-w-[900px] w-full mx-auto mt-2 text-center font-poppins leading-relaxed"
         >
           Kickstart your data career with <b>DataCliq</b> – the ultimate platform for aspiring data analysts!
           Gain expertise in tools like Excel, SQL, and Tableau, and build real-world projects to stand out.
@@ -58,14 +58,43 @@ const HeroSection = () => {
           className="flex flex-col sm:flex-row gap-4 justify-center mt-8"
         >
           {/* Filled Button */}
-          <button className="bg-[#F4A500] text-white font-poppins font-semibold py-3 px-8 rounded-full ">
+          <button
+            type="submit"
+            class="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-[#F4A500] backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group"
+          >
             Explore Courses
+            <svg
+              class="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-2 rotate-45"
+              viewBox="0 0 16 19"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
+                class="fill-gray-800 group-hover:fill-gray-800"
+              ></path>
+            </svg>
           </button>
 
           {/* Outlined Button */}
-          <button className="border-2 border-[#FFC300] text-[#FFC300] font-poppins font-semibold py-3 px-8 rounded-full hover:bg-[#ffffff] hover:text-[#FFC300] transition-colors duration-300">
-            Download Cheatsheet
-          </button>
+          
+          {/* <div
+            class="flex justify-center items-center border-2 border-radius border-white-200 overflow-hidden p-1 rounded-full shadow-lg"
+          >
+            <button
+              class="bg-[linear-gradient(#e9e9e9,#e9e9e9_50%,#fff)] group w-50 h-16 inline-flex transition-all duration-300 overflow-visible p-1 rounded-full group"
+            >
+              <div
+                class="w-full h-full bg-[linear-gradient(to_top,#ececec,#fff)] overflow-hidden shadow-[0_0_1px_rgba(0,0,0,0.07),0_0_1px_rgba(0,0,0,0.05),0_3px_3px_rgba(0,0,0,0.25),0_1px_3px_rgba(0,0,0,0.12)] p-1 rounded-full hover:shadow-none duration-300"
+              >
+                <div
+                  class="w-full h-full text-xl gap-x-0.5 gap-y-0.5 justify-center text-[#101010] bg-[linear-gradient(#f4f4f4,#fefefe)] group-hover:bg-[linear-gradient(#e2e2e2,#fefefe)] duration-200 items-center text-[18px] font-medium gap-4 inline-flex overflow-hidden px-4 py-2 rounded-full black group-hover:text-blue-600 "
+                >
+                  <span class="ml-2">Download Cheatsheet</span>
+                </div>
+              </div>
+            </button>
+          </div> */}
+
         </motion.div>
       </div>
     </div>
